41179c870167004b15b84a4530b17b1ba540f8b4
zengyanling <939184663@qq.com>
2017-04-28 17:08:26 +0800

    refactor: init

---
+++ view/templates/_index.vue
@@ -0,0 +1,22 @@
+/*
+ * @Author: yourName
+ * @Date: 2017-04-28 16:20:38
+ * @Last Modified by: zengyanling
+ * @Last Modified time: 2017-04-28 16:55:41
+ */
+
+<template src="./<%= cmpName %>.html"></template>
+<script>
+export default {
+    data() {
+        return {
+        };
+    },
+    methods: {
+        test(){
+
+        }
+    }
+};
+</script>
+<style lang="scss" src="./<%= cmpName %>.scss"></style>
