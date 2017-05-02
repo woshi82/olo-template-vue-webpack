8e41681b2181f04a398d4106f3a50915aabdf46a
zengyanling <939184663@qq.com>
2017-05-02 15:43:45 +0800

    fix: spelling error

--- view/templates/_index.vue
+++ view/templates/_index.vue
@@ -2,10 +2,10 @@
  * @Author: yourName
  * @Date: 2017-04-28 16:20:38
  * @Last Modified by: zengyanling
- * @Last Modified time: 2017-04-28 16:55:41
+ * @Last Modified time: 2017-05-02 15:43:12
  */
 
-<template src="./<%= cmpName %>.html"></template>
+<template src="./<%= viewName %>.html"></template>
 <script>
 export default {
     data() {
@@ -19,4 +19,4 @@ export default {
     }
 };
 </script>
-<style lang="scss" src="./<%= cmpName %>.scss"></style>
+<style lang="scss" src="./<%= viewName %>.scss"></style>
